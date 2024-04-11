08d510e8560d5ee7a7751c266665a5be9244cb40
rodolfostark <rodolfojonatas@outlook.com>
2024-04-10 22:24:19 -0300

    test: list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, test, describe } from 'vitest'
+import { afterAll, beforeAll, test, describe, it } from 'vitest'
 import request from 'supertest'
 
 import { app } from '../src/app'
@@ -12,7 +12,7 @@ describe('Transatcions routes', () => {
     await app.close()
   })
 
-  test('The user can create a new transaction', async () => {
+  it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -21,5 +21,20 @@ describe('Transatcions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+    const cookies = createTransactionResponse.get('Set-Cookie')
+    await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
   })
 })
